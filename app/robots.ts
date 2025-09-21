a800fd2b7a3d65b6aada66bd6f49b33cca5e1fbb
qj365 <quangjunior365@gmail.com>
2025-09-21 08:42:07 +0700

    fix robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -4,13 +4,7 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            allow: '/',
-            disallow: [
-                '/api/', // Protect API routes
-                '/admin/', // Protect admin routes if any
-                '/_next/', // Protect Next.js system files
-                '/private/', // Protect private content if any
-            ],
+            disallow: '',
         },
         sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
         host: process.env.NEXT_PUBLIC_BASE_URL,
